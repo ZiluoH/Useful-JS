12187b5bfc879409d126bdb0f489cfce7f111ed1
ZiluoH <ziluoh@gmail.com>
2018-05-20 21:05:26 -0700

    Add files via upload

--- JS Practice/4-DOM-Pig-Game/app.js
+++ JS Practice/4-DOM-Pig-Game/app.js
@@ -13,9 +13,28 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 0;
-
-dice = Math.floor(Math.random() * 6) + 1;
+activePlayer = 1;
 
 
-document.querySelector('#current-0').textContent = dice;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	// Random Number
+	var dice = Math.floor(Math.random() * 6) + 1;
+
+	// Display the result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+
+	// update the round score IF the rolled number is NOT 1
+})
